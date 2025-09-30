f2cfb8741c9949c7b9fb99159838af99c63909cc
VascPT <vascorf2008@gmail.com>
2025-09-30 11:31:06 +0000

    exerciciosaula

---
+++ exerciciosaula.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//cria uma funcao que aceite como parametro uma estrutura e mostre o seu conteudo
+//a ter em consideracao:
+//-a estrutura ao ser passada na funcao ja deve ter dados
+//-a estrutura tem o seguinte formato:
+//nome: carro
+//atributos: string Marca,
+ //  string Modelo
+
+    struct Carro{
+        string marca;
+        string modelo;
+    };
+void myFunction(Carro c){
+    cout << "Marca: " << c.marca << ", Modelo: " << c.modelo << endl;
+}
+
+int main(){
+    Carro myCar = {"BMW", "chibidibaum"};
+    myFunction(myCar);
+    return 0;
+}
