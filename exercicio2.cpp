7122adbdff10fa030b39c13dc7fd1896477abfc7
VascPT <vascorf2008@gmail.com>
2025-10-07 11:25:51 +0000

    exercicio2

--- exercicio2.cpp
+++ exercicio2.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
 
+struct Trabalhadores{
+    string nome;
+    int idade;
+};
+
+struct Loja{
+    string nomeprodutos;
+    int numdecaixas;
+    Trabalhadores trabalhadores;
+};
+
+Loja lojadetalhes = {"bananas", 4, {"joao", 26}};
+
+int main(){
+    cout << lojadetalhes.nomeprodutos << endl;
+    cout << lojadetalhes.numdecaixas << endl;
+    cout << lojadetalhes.trabalhadores.idade << endl;
+    cout << lojadetalhes.trabalhadores.nome << endl;
+    return 0;
+}
