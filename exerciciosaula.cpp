f8583b7aa65e08ed28dedcd48fafa9c8079a1e94
VascPT <vascorf2008@gmail.com>
2025-10-07 11:06:22 +0000

    .vscode/settings

--- exerciciosaula.cpp
+++ exerciciosaula.cpp
@@ -24,7 +24,7 @@ int main(){
     return 0;
 }*/
 
-struct Escola{
+/*struct Escola{
     string nome;
     string morada;
 };
@@ -40,5 +40,33 @@ Aluno a1 = {"Roberto", 20, "autista", {"domingos rebelo", "Ponta Delgada"}};
 
 int main() {
     cout << a1.escola.morada;
+    return 0;
+}*/
+struct Escola{
+    string nome;
+    string morada;
+};
+
+struct Aluno{
+    string nome;
+    int idade;
+    string caracteristicas;
+    Escola escola;
+};
+
+Aluno a1;
+
+int main() {
+    cout << "Introduza o nome do aluno: " << endl;
+    cin >> a1.nome;
+    cout << "Introduza a idade: " << endl;
+    cin >> a1.idade;
+    cout << "Introduza uma caracteristida: " << endl;
+    cin >> a1.caracteristicas;
+    cout << "Qual o nome da escola do aluno?: " << endl;
+    cin >> a1.escola.nome;
+    cout << "Qual o sitio da escola?: " << endl;
+    cin >> a1.escola.morada;
+    cout << endl << a1.nome << endl << a1.idade << endl << a1.caracteristicas << endl << a1.escola.nome << endl << a1.escola.morada << endl;
     return 0;
 }
