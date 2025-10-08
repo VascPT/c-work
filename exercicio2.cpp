2895a04d93648e6b90d181821c4c3169c4a803a8
VascPT <vascorf2008@gmail.com>
2025-10-08 12:31:29 +0000

    exericio2

--- exercicio2.cpp
+++ exercicio2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
 
 /*struct Trabalhadores{
@@ -25,5 +27,20 @@ using namespace std;
 
 
 int main(){
+    system("clear");
+    int x;
+    cout << "How old are you?" << endl;
+    cin >> x;
+    if(x==1){
+    cout << endl << "I am " << x << " year old!" << endl;
+}else if (x>= 2 || x<= 0){
+    cout << endl << "I am " << x << " years old!" << endl;
+}
+this_thread::sleep_for(chrono::seconds(2));
+    if(x==4){
+        cout << endl << "You are the youngest person ever!" << endl << endl;
+    }else{
+        cout << endl << "Fuck you" << endl << endl;
+    }
     return 0;
 }
