b51f802983c8bdb4aa0a7f296b6fcbdcb9d0771f
VascPT <vascorf2008@gmail.com>
2025-10-28 12:26:53 -0100

    personal/ownwork.cpp

--- personal/ownwork.cpp
+++ personal/ownwork.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
+void clear(){
+    #ifdef WIN32
+        system("CLS");
+    #else
+        system("clear");
+    #endif
+}
+
+
+int main(){
+    clear();
+    float age;
+    cout << "What is your age? ";
+    cin >> age;
+    sleep(1);
+    clear();
+    cout << "Oh, you are " << age << " years old?" << endl;
+    sleep(1);
+    while (age != 67 || 69 || 21 || 41){
+        cout << "Wrong! Try again! " << endl;
+    }
+}
