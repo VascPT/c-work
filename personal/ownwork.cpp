4dba8bb32c639b7d17edd624ea7b9118cf360298
VascPT <vascorf2008@gmail.com>
2025-10-31 10:03:31 -0100

    personal/

--- personal/ownwork.cpp
+++ personal/ownwork.cpp
@@ -9,17 +9,58 @@ void clear(){
     #endif
 }
 
+float age;
 
-int main(){
+void question(){
     clear();
-    float age;
+    do{
     cout << "What is your age? ";
     cin >> age;
     sleep(1);
     clear();
+    if (age == 1){
+        cout << "Oh, you are " << age << " year old?" << endl; 
+    }else{
     cout << "Oh, you are " << age << " years old?" << endl;
+}
+
     sleep(1);
-    while (age != 67 || 69 || 21 || 41){
-        cout << "Wrong! Try again! " << endl;
+    
+    if (age == 67){
+        sleep(1);
+        clear();
+        cout << age << "!!!!!!!!!!!" << endl;
+        break;
+    }else if (age == 69){
+        cout << age << "?" << endl;
+        sleep(1);
+        cout << "nice";
+        break;
+    }else if (age == 21){
+        sleep (1);
+        clear();
+        cout << "u stoopid" << endl;
+        break;
+    }else if (age == 41){
+        sleep(1);
+        clear();
+        cout << "do you also have 41 gold?" << endl;
+        break;
     }
+
+    if (age != 67 || 69 || 21 || 41){
+        sleep(1);
+        clear();        
+        cout << "Wrong! Try again!" << endl;
+        sleep(1);
+    }
+}while (age != 67 || 69 || 21 || 41);
+
+
+}
+
+int main(){
+    clear();
+    question();
+
 }
