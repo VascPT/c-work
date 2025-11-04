e90ae20714124d5667d5581b93c8ad90d1be7396
VascPT <vascorf2008@gmail.com>
2025-11-04 12:07:09 -0100

    JogodaGloria.cpp

--- JogodaGloria.cpp
+++ JogodaGloria.cpp
@@ -1,4 +1,4 @@
-//Código feito por Vasco
+//Código feito por Vasco Franco
 #include <iostream>
 #include <limits>
 using namespace std;
@@ -8,6 +8,29 @@ void clear(){
     #else
         system("clear");
     #endif
+}
+const int N = 30;
+const int Perde_Jogada = 4; //casa 4
+const int Volta_tres_casas = 6; //casa 6
+const int Avanca_tres_casas = 2; //casa 2
+const int Joga_novament = 14; //casa 14
+const int Espera_passg = 23; // casa 23
+
+void iniciarCasas(string casas[N]){
+    for (int i=0; i<N; i++)
+        casas[i] = '-';
+    casas[Perde_Jogada] = "PJ"; //Perde a vez
+    casas[Volta_tres_casas] = "V3"; //Volta 3 casas
+    casas[Avanca_tres_casas] = "A3"; //Avança 3 casas
+    casas[Joga_novament] = "JN"; //Joga novamente
+    casas[Espera_passg] = "EP"; //Espera passagem do próximo jogador
+}
+
+void imprimirCasas(string casas[N]){
+    for(int i=0 ; i<N;i++){
+        cout<<(i+1<10?" ":"")<<i+1<<" ";
+    }
+
 }
 
 
