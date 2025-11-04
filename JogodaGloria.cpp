71fa9ecec2a5be5cf9d00735d92466fd4defa8f1
VascPT <vascorf2008@gmail.com>
2025-11-04 12:23:57 -0100

    JogodaGloria.cpp

--- JogodaGloria.cpp
+++ JogodaGloria.cpp
@@ -1,5 +1,7 @@
 //Código feito por Vasco Franco
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <limits>
 using namespace std;
 void clear(){
@@ -11,14 +13,14 @@ void clear(){
 }
 const int N = 30;
 const int Perde_Jogada = 4; //casa 4
-const int Volta_tres_casas = 6; //casa 6
-const int Avanca_tres_casas = 2; //casa 2
-const int Joga_novament = 14; //casa 14
+const int Volta_tres_casas = 10; //casa 10
+const int Avanca_tres_casas = 5; //casa 5
+const int Joga_novament = 15; //casa 15
 const int Espera_passg = 23; // casa 23
 
 void iniciarCasas(string casas[N]){
     for (int i=0; i<N; i++)
-        casas[i] = '-';
+        casas[i] = '---';
     casas[Perde_Jogada] = "PJ"; //Perde a vez
     casas[Volta_tres_casas] = "V3"; //Volta 3 casas
     casas[Avanca_tres_casas] = "A3"; //Avança 3 casas
@@ -32,7 +34,11 @@ void imprimirCasas(string casas[N]){
     }
 
 }
-
+void dado(){
+    std::srand(std::time(nullptr)); // inicializa o gerador com o tempo atual
+    int numero = std::rand() % 6 + 1; // número entre 1 e 6 (como um dado)
+    std::cout << "Saiu o número: " << numero << std::endl;
+}
 
 int main(){
     clear();
