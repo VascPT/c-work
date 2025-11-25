3c78b528b16fbfac1252cbf0d15c94364cbad9c1
VascPT <vascorf2008@gmail.com>
2025-11-25 12:07:34 -0100

    bingo_vector

---
+++ bingo_vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define TOTAL_NUMS 50
+void clear(){
+    #ifdef WIN32
+        system("CLS");
+    #else
+        system("clear");
+    #endif
+}
+
+vector<int> sorteioNums;
+
+
+int main(){
+
+    return 0;
+}
