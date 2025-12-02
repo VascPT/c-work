e643fd96b7091d0929df928d02e381743d4839e0
VascPT <vascorf2008@gmail.com>
2025-12-02 12:32:39 -0100

    bingo_numGen.cpp

--- bingo_numGen.cpp
+++ bingo_numGen.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 #define TOTAL_NUMS 100
 #define PER_LINE 20
+//colors for numbers
 #define RESET "\033[0m"
 #define RED "\033[31m"
 #define BLUE "\033[34m"
@@ -38,6 +39,9 @@ int main(){
     clear();
     cout << endl << "Welcome to BINGO! Here is the table of numbers. Good luck!" << endl << endl;
     showNumbers();
+    cout << RESET << "Press enter to contnue" << endl;
+    //press enter to continue
+    cin.ignore();
 
     return 0;
 }
