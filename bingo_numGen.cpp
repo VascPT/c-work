ba1cbcab3107d0818005266460cd3d3be0d96c32
VascPT <vascorf2008@gmail.com>
2025-12-02 11:40:10 -0100

    bingo_numGen.cpp

---
+++ bingo_numGen.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define TOTAL_NUMS 100
+#define PER_LINE 20
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define BLUE "\033[34m"
+#define WHITE "\033[37m"
+#define CYAN "\033[36m"
+#define YELLOW "\033[33m"
+#define BGGRAY "\033[100m"
+#define PURPLE "\e[0;35m"
+void clear(){
+    #ifdef WIN32
+        system("CLS");
+    #else
+        system("clear");
+    #endif
+}
+
+void showNumbers(){
+    //shows all the 100 nums
+    for(int i=1; i<=TOTAL_NUMS; i++){
+        cout << i << " ";
+        //Makes lines of 20
+        if (i % PER_LINE == 0){
+        cout << "\n";
+    }
+    }
+}
+
+
+int main(){
+    clear();
+    cout << endl << "Welcome to BINGO! Here is the table of numbers. Good luck!" << endl << endl;
+    showNumbers();
+
+    return 0;
+}
