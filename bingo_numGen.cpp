bac2fddd49f4ef687cffc39f64f4efa452468552
VascPT <vascorf2008@gmail.com>
2025-12-05 09:08:27 -0100

    update

--- bingo_numGen.cpp
+++ bingo_numGen.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#ifdef WIN32
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#ifdef WIN32_
         #include <windows.h>
     #else
         #include <unistd.h>
@@ -22,26 +25,112 @@ void clear(){
         system("clear");
     #endif
 }
+//track numbers that have been drawn
+bool drawn[TOTAL_NUMS + 1] = {false};
+//store up to 5 numbers
+int recentNumbers[5];
+//how many numbers are currently stored
+int recentCount = 0;
 
 void showNumbers(){
     //shows all the 100 nums
     for(int i=1; i<=TOTAL_NUMS; i++){
-        cout << CYAN << i << " ";
+       if (drawn[i]) {
+            //drawn nums
+            cout << YELLOW << i << " ";
+        } else {
+            //regular nums
+            cout << CYAN << i << " ";
+        }
         //Makes lines of 20
         if (i % PER_LINE == 0){
         cout << "\n";
     }
     }
 }
+void addtoRecent(int num) {
+    //shift all numbers to the left making room at the end
+    //removes the oldest number
+    for (int i = 0; i < 4; i++) {
+        recentNumbers[i] = recentNumbers[i + 1];
+    }
+    //add new number at the end (most recent)
+    recentNumbers[4] = num;
+    //update count (max at 5)
+    if (recentCount < 5) {
+        recentCount++;
+    }
+}
 
+void showRecentNumbers() {
+    if (recentCount == 0) {
+        cout << PURPLE << "Recent numbers (last 5): None yet" << RESET;
+        return;
+    }
+    
+    cout << PURPLE << "Recent numbers (last 5): ";
+    
+    //only show the actual numbers we have
+    int startIndex = 5 - recentCount;//show from where we have valid data
+    for (int i = startIndex; i < 5; i++) {
+        cout << recentNumbers[i];
+        if (i < 4) {
+            cout << ", ";
+        }
+    }
+    cout << RESET;
+}
+
+int drawrandNum(){
+    vector<int> pool;
+    //list of nums that have not been drawn yet
+     for (int i = 1; i <= TOTAL_NUMS; i++) {
+        if (!drawn[i]) {
+            pool.push_back(i);
+        }
+    }
+
+    if (pool.empty())
+        return -1; //no more nums left
+
+    int index = rand() % pool.size();
+    int num = pool[index];
+
+    drawn[num] = true; //mark number as drawn (yellow)
+    addtoRecent(num);
+    return num;
+}
 
 int main(){
+    srand(time(NULL));
     clear();
     cout << endl << "Welcome to BINGO! Here is the table of numbers. Good luck!" << endl << endl;
     showNumbers();
-    cout << RESET << "Press enter to contnue" << endl;
+    //initialize recent numbers array
+    for (int i = 0; i < 5; i++) {
+        recentNumbers[i] = 0;  //0 means empty slot
+    }
+    cout << RESET << "Press enter to rool a number ";
     //press enter to continue
     cin.ignore();
+     while (true) {
+        clear();
+
+        int num = drawrandNum();
+
+        if (num == -1) {
+            cout << YELLOW << "\nAll numbers have been drawn!" << RESET << endl;
+            break;
+        }
+
+        cout << "Number drawn: " << YELLOW << num << RESET << "\n\n";
+
+        showNumbers();
+        cout << "\n";
+        showRecentNumbers();
+        cout << RESET << "\nPress ENTER to draw again ";
+        cin.ignore();
+    }
 
     return 0;
 }
