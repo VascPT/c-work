a4fd65e3ea4c0fb8c2ca84a6e14f5ae8f359a7c0
VascPT <vascorf2008@gmail.com>
2025-12-10 18:58:13 -0100

    cartao

--- geraCartaobingo.cpp
+++ geraCartaobingo.cpp
@@ -7,42 +7,54 @@
 
 using namespace std;
 
-int main() {
-    srand(time(NULL));
-    
-    //PEDIR quantos cartões quer gerar
-    int num_cartoes;
-    cout << "Quantos cartoes de Bingo quer gerar? (1 a 500): ";
-    cin >> num_cartoes;
-    
-    // Validar entrada
-    if (num_cartoes < 1 || num_cartoes > 500) {
-        cout << "Numero invalido! Deve ser entre 1 e 500." << endl;
+int main(int argc, char* argv[]) {
+    //verificar se foi fornecido o número de cartões
+    if (argc != 2) {
+        cout << "Uso: ./geraCartaobingo [numero_de_cartoes]" << endl;
+        cout << "Exemplo: ./geraCartaobingo 10" << endl;
+        cout << "Minimo: 1 cartao, Maximo: 500 cartoes" << endl;
         return 1;
     }
     
+    //converter argumento para número
+    int num_cartoes = atoi(argv[1]);
+    
+    //validar entrada
+    if (num_cartoes < 1 || num_cartoes > 500) {
+        cout << "ERRO: Numero de cartoes deve estar entre 1 e 500!" << endl;
+        return 1;
+    }
+    
+    //inicializar gerador aleatório
+    srand(time(NULL));
+    
+    //intervalos para cada coluna B-I-N-G-O
     int minimos[5] = {1, 21, 41, 61, 81};
     int maximos[5] = {20, 40, 60, 80, 100};
     
-    //Gerar múltiplos cartões
+    cout << "A gerar " << num_cartoes << " cartoes de Bingo..." << endl;
+    
+    //gerar cada cartão
     for (int cartao_num = 1; cartao_num <= num_cartoes; cartao_num++) {
-        //CRIAR nome do ficheiro
+        //criar nome do ficheiro
         string nome_ficheiro = "cartao" + to_string(cartao_num) + ".txt";
         
-        //ABRIR ficheiro para este cartão
+        //abrir ficheiro
         ofstream arquivo(nome_ficheiro);
         
         if (!arquivo.is_open()) {
-            cout << "Erro ao criar ficheiro " << nome_ficheiro << "!" << endl;
-            continue;  //Tenta o próximo cartão
+            cout << "Erro ao criar ficheiro: " << nome_ficheiro << endl;
+            continue;
         }
         
+        // Matriz para o cartão
         int cartao[5][5];
         
-        //Gera números para cada coluna
+        //gerar números para cada coluna
         for (int coluna = 0; coluna < 5; coluna++) {
             vector<int> numeros_coluna;
             
+            //gerar 5 números únicos para esta coluna
             while (numeros_coluna.size() < 5) {
                 int numero = minimos[coluna] + rand() % (maximos[coluna] - minimos[coluna] + 1);
                 
@@ -59,21 +71,24 @@ int main() {
                 }
             }
             
+            //colocar números na matriz
             for (int linha = 0; linha < 5; linha++) {
                 cartao[linha][coluna] = numeros_coluna[linha];
             }
         }
         
-        //ESCREVER no ficheiro
-        arquivo << "Cartao de Bingo #" << cartao_num << "\n";
+        //escrever cartão no ficheiro
+        arquivo << "Cartao de Bingo #" << cartao_num << "\n\n";
         arquivo << " B    I    N    G    O\n";
         arquivo << "----------------------\n";
         
         for (int linha = 0; linha < 5; linha++) {
             for (int coluna = 0; coluna < 5; coluna++) {
+                //espaço livre no centro
                 if (coluna == 2 && linha == 2) {
                     arquivo << " ** ";
                 } else {
+                    //formatar com 2 dígitos
                     if (cartao[linha][coluna] < 10) {
                         arquivo << "  " << cartao[linha][coluna] << " ";
                     } else {
@@ -84,15 +99,17 @@ int main() {
             arquivo << endl;
         }
         
-        //FECHAR este ficheiro
         arquivo.close();
-        
-        //Mostrar no terminal também
-        cout << "Cartao " << cartao_num << " criado: " << nome_ficheiro << endl;
+        cout << "Cartao " << cartao_num << " -> " << nome_ficheiro << endl;
     }
     
-    cout << "\n----------------------------------------\n";
-    cout << "Foram criados " << num_cartoes << " ficheiros:\n";
+    cout << "\n----------------------------------------" << endl;
+    cout << "GERACAO CONCLUIDA!" << endl;
+    cout << "Total de cartoes gerados: " << num_cartoes << endl;
+    cout << "Ficheiros criados: cartao1.txt a cartao" << num_cartoes << ".txt" << endl;
+    
+    return 0;
+}n";
     cout << "cartao1.txt, cartao2.txt, ... cartao" << num_cartoes << ".txt\n";
     
     return 0;
