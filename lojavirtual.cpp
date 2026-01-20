70c717c1b71f273e28d644f0affa21eca56fa43d
VascPT <vascorf2008@gmail.com>
2026-01-20 12:23:37 -0100

    .

---
+++ lojavirtual.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Attribute{
+    private:
+    int prodName;
+    float prodPrice;
+
+    public:
+
+    Attribute(int name, float price) : prodName(name), prodPrice(price) {}
+
+    float getPrice(){
+        price -> prodPrice;
+    }
+};
+
+int main(){
+    
+}
