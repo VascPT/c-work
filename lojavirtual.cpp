67716d9f824d1ef5e57e78b25aa64589115c3615
VascPT <vascorf2008@gmail.com>
2026-01-20 12:36:40 -0100

    .

--- lojavirtual.cpp
+++ lojavirtual.cpp
@@ -8,13 +8,24 @@ class Attribute{
 
     public:
 
-    Attribute(int name, float price) : prodName(name), prodPrice(price) {}
+    void setPrice(double p){
+        if (p < 0) {  //verifica se o preço é negativo
+            prodPrice = 0;  //define como 0
+            //avisar o usuário
+            cout << "Atenção: Preço negativo não permitido! Definido como 0." << endl;
+        } else {
+            prodPrice = p;  //se não for negativo, usa o valor normal
+        }
+    }
 
     float getPrice(){
-        price -> prodPrice;
+        return prodPrice;
     }
 };
 
 int main(){
-    
+    Attribute product1;
+    product1.setPrice(20);
+    cout << product1.getPrice() << endl;
+
 }
