7394156ae6e77840432f51d80aecf046ee7b181f
VascPT <vascorf2008@gmail.com>
2026-01-22 19:54:47 -0100

    e

--- lojavirtual.cpp
+++ lojavirtual.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 using namespace std;
+void clear(){
+    #ifdef WIN32
+        system("CLS");
+    #else
+        system("clear");
+    #endif
+}
 
-class Attribute{
+class Product{
     private:
-    int prodName;
+    string prodName;
     float prodPrice;
 
     public:
 
-    void setPrice(double p){
-        if (p < 0) {  //verifica se o preço é negativo
+    void setName(string n1){
+        prodName = n1;
+    }
+
+    string getName(){
+        return prodName;
+    }
+
+    void setPrice(double p1){
+        if (p1 < 0) {  //verifica se o preço é negativo
             prodPrice = 0;  //define como 0
-            //avisar o usuário
-            cout << "Atenção: Preço negativo não permitido! Definido como 0." << endl;
+            cout << "Warning: Price can't be negate! Automatically set to 0." << endl;
         } else {
-            prodPrice = p;  //se não for negativo, usa o valor normal
+            prodPrice = p1;  //se não for negativo, usa o valor normal
         }
     }
 
@@ -23,9 +37,35 @@ class Attribute{
     }
 };
 
+class Author: public Product{//classe derivada
+    public:
+    string author = "Joao";
+};
+
+class Brand: public Product{
+    public:
+    string brand = "Xiaomi";  
+};
+
+
 int main(){
-    Attribute product1;
-    product1.setPrice(20);
-    cout << product1.getPrice() << endl;
+    clear();
+    Author livro;
+    Brand phone;
+    livro.setName("Maias");
+    livro.setPrice(20);
+    phone.setName("Phone x");
+    phone.setPrice(400);
+
+    cout << "Store registration below:" << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "\t\t" << livro.getName() << endl;
+    cout << "\t\t" << livro.getPrice() << " Euros" << endl;
+    cout << "\t" << "Author of book: " << livro.author << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "\t\t" << phone.getName() << endl;
+    cout << "\t\t" << phone.getPrice() << " Euros" << endl;
+    cout << "\t" << "Phone's brand: " << phone.brand << endl;
+    cout << "-------------------------------------" << endl;
 
 }
