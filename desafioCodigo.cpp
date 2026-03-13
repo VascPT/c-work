42349cf38d44a846603d4fb14595e51b3f3c8004
Vasco Franco <vascorf2008@gmail.com>
2026-03-13 10:09:33 -0100

    aa

--- desafioCodigo.cpp
+++ desafioCodigo.cpp
@@ -7,6 +7,7 @@ long temperature = 0;
 #define SET_POINT 45
 
 long randomizeTemperature(){
+    //automatically randomize temperature between 1-100
     temperature = rand() % 100;
     cout << "Temperature: " << temperature << endl;
     
@@ -22,14 +23,14 @@ void aC(){
 }
 
 int main(){
+    //randomize seed
     srand(time(0));
-    //delay of 4 seconds
+    //delay of 4 seconds, total of 10 times
     for(int i = 0; i < 10; i++){
     randomizeTemperature();
     aC();
     this_thread::sleep_for(chrono::seconds(4));
     }
 
-  
     return 0;
 }
